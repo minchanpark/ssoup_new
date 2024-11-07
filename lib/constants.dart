5e1f6fe2f72c08def1e094a79016b0bddbceb690
minchanpark <alscksqkr13@gmail.com>
2024-11-08 00:18:15 +0900

    first commit

--- lib/constants.dart
+++
@@ -1,4 +0,0 @@
-const String naverClientId = "y1j4kgfwpv";
-const String naverClientSecret = "0Lr7EBoqCleZY1bnXinUcgug91iFTEuj6JNIsXI6";
-const String kakaoNativeAppKey = "0e922928043e9a70eed8cfb92e43a30a";
-const String kakaoJavaScriptAppKey = "44a80917397ae2773e2b30ce7d9e7d72";
